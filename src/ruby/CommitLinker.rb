87636404e47669ab09bd0d4ae8857169d1ed66f1
jbranchaud <jbranchaud@gmail.com>
2013-05-11 18:38:45 -0500

    Modularizing the code in CommitLinker.rb so that it can be invoked as a script or used by another script.

--- src/ruby/CommitLinker.rb
+++ src/ruby/CommitLinker.rb
@@ -6,6 +6,35 @@
 
 require 'grit'
 
+# main
+#
+# this function is only invoked if this script is being invoked directly.
+# It is given the command-line arguments which should be made up of 3
+# things:
+# - local repository path
+# - GitHub username for repository owner
+# - GitHub repository name
+def main(args)
+
+  # check that 3 arguments have been given, otherwise exit
+  if ARGV.length < 3
+    puts "You haven't given enough arguments."
+    exit
+  end
+
+  # the first argument should be the path to the local repository
+  repo_path = ARGV[0]
+
+  # the second argument should be the relevant GitHub username
+  username = ARGV[1]
+
+  # the third argument should be the repository name on GitHub
+  repo_name = ARGV[2]
+
+  # invoke link_commits with the arguments
+  link_commits(repo_path, username, repo_name).each { |link| puts link }
+end
+
 # get_all_commits
 #
 # given a Grit::Repo object, this function will build a list of all commits
@@ -24,28 +53,32 @@ def get_all_commits(repo)
   return all_commits
 end
 
-if ARGV.length < 3
-  puts "You haven't given enough arguments."
-  exit
+# link_commits
+#
+# given the path to a local git repository, a GitHub username for the
+# repository owner, and the name of the GitHub repository, this function
+# will build a list of markdown links to each commit SHA and return that
+# list.
+def link_commits(repo_path, username, repo_name)
+
+  # create a partial repo URL for GitHub repository
+  partial_URL = "https://github.com/#{username}/#{repo_name}/commit/"
+  
+  # access the repository at the given repo_path
+  repo = Grit::Repo.new(repo_path)
+  
+  all_commits = get_all_commits(repo)
+  
+  #all_commits.each{ |commit|
+  #  puts "[#{commit.sha}](#{partial_URL + commit.sha})\n\n"
+  #}
+
+  return all_commits.map{ |commit|
+    "[#{commit.sha}](#{partial_URL + commit.sha})\n\n"
+  }
 end
 
-# the first argument should be the path to the local repository
-repo_path = ARGV[0]
-
-# the second argument should be the relevant GitHub username
-username = ARGV[1]
-
-# the third argument should be the repository name on GitHub
-repo_name = ARGV[2]
-
-# create a partial repo URL for GitHub repository
-partial_URL = "https://github.com/#{username}/#{repo_name}/commit/"
-
-# access the repository at the given repo_path
-repo = Grit::Repo.new(repo_path)
-
-all_commits = get_all_commits(repo)
-
-all_commits.each{ |commit|
-  puts "[#{commit.sha}](#{partial_URL + commit.sha})\n\n"
-}
+# if this is being invoked as a script, invoke the main method
+if __FILE__==$0
+  main(ARGV)
+end
